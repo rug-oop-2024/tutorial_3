4f7e9f95080e0434338403121b4b02a697698263
Mo Assaf <mormoassaf@gmail.com>
2024-10-22 10:12:27 +0200

    html demo

--- app/pages/HTML.py
+++ app/pages/HTML.py
@@ -1,6 +1,9 @@
 
 import streamlit as st
 import pygwalker as pyg
+from pygwalker.api.streamlit import StreamlitRenderer
+import pandas as pd
+from sklearn.datasets import fetch_openml
 
 
 # HTML showcase
@@ -17,10 +20,23 @@ def app():
     st.markdown('<p style="color:red;">This is a red paragraph</p>', unsafe_allow_html=True)
     st.markdown('<a href="https://www.streamlit.io/">This is a link</a>', unsafe_allow_html=True)
 
-    # Add a Pygame
-    st.markdown('## Pygame')
-    st.markdown('This is a Pygame')
+    # Showcase pygwalker
+    st.markdown('## Pygwalker')
+    st.markdown('### Pygwalker is a simple Python package that allows you to create a walking animation of a text.')
+    st.markdown('### To install pygwalker, use the following command:')
+    st.code('pip install pygwalker')
+    st.markdown('### To use pygwalker, use the following code:')
+    
+    # now lets integrate pygwalker
+    data = fetch_openml(name="adult", version=1, parser="auto")
+    df = pd.DataFrame(
+        data.data,
+        columns=data.feature_names,
+    )
+    st.write(df.head())
+    pyg_app = StreamlitRenderer(df)
+    pyg_app.explorer()
+            
 
-    # Add a Pygame
-    st.markdown('## Pygame')
-    st.markdown('This is a Pygame')
+
+app()
